e2fed8fbe67e68a1538b63aef16c87648690b255
VeroniJrStudant <veroni_vieira-jun1@estudante.sesisenai.org.br>
2022-09-23 00:26:12 -0300

    Inserido script js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// ATIVAR LINKS DO MENU
+const links = document.querySelectorAll(".header-menu a");
+
+//MÉTODO .includes("a") VERIFICAR SE UMA STRING EXISTE EM JAVASCRIPT
+console.log("javascript".includes("a"));
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  url.includes;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+    console.log(link);
+  }
+
+  console.log(url);
+  console.log(href);
+}
+
+links.forEach(ativarLink);
+
+// ITENS DO ORÇAMENTO
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  elemento.checked = true;
+}
+
+parametros.forEach(ativarProduto);
